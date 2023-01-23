cdf164ca3a4f06642f03cf5633e3ac89b733d8d1
Saba <saba.sadegh67@gmail.com>
2023-01-23 20:07:05 +0330

    customize a hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,14 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import useFetch from "../useFetch";
+
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-  useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        if (!res.ok) {
-          throw Error("could'n fetch data");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-        setIsPending(false);
-        setError(null);
-      })
-      .catch((err) => {
-        setIsPending(false);
-        setError(err.message);
-      });
-  }, []);
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
   return (
     <div className="home">
       {error && <div>{error}</div>}
